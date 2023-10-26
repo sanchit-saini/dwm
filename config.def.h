cfa55023d8c3de2af961a86f34237103080ee0d8
Sanchit Saini <sanchitsaini14@gmail.com>
2023-10-26 12:25:20 +0530

    Move colors into its own file, remap Mod+m, update workspace icons

--- config.def.h
+++ config.def.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include "colors.h"
+
 /* appearance */
 static const unsigned int colorfultag    = 1;  /* 0 means use SchemeSel for selected tag */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -19,38 +21,6 @@ static const char *fonts[]          = { "monospace:size=14",
                                         "Material Design Icons-Regular:size=10" };
 
 /* one dark */
-static const char black[]       = "#1e222a";
-static const char gray2[]       = "#2e323a"; // unfocused window border
-static const char gray3[]       = "#545862";
-static const char gray4[]       = "#6d8dad";
-static const char blue[]        = "#61afef";  // focused window border
-static const char green[]       = "#7EC7A2";
-static const char red[]         = "#e06c75";
-static const char orange[]      = "#caaa6a";
-static const char yellow[]      = "#EBCB8B";
-static const char pink[]        = "#c678dd";
-static const char col_borderbar[]  = "#1e222a"; // inner border
-
-static const char col_bg[]          = "#1a1b26";
-static const char col_dark[]        = "#16161E";
-static const char col_fg[]          = "#a9b1d6";
-static const char col_black[]       = "#32344a";
-static const char col_red[]         = "#f7768e";
-static const char col_green[]       = "#9ece6a";
-static const char col_yellow[]      = "#e0af68";
-static const char col_blue[]        = "#7aa2f7";
-static const char col_magenta[]     = "#ad8ee6";
-static const char col_cyan[]        = "#449dab";
-static const char col_white[]       = "#787c99";
-static const char col_br_black[]    = "#444b6a";
-static const char col_br_red[]      = "#ff7a93";
-static const char col_br_green[]    = "#b9f27c";
-static const char col_br_yellow[]   = "#ff9e64";
-static const char col_br_blue[]     = "#7da6ff";
-static const char col_br_magenta[]  = "#bb9af7";
-static const char col_br_cyan[]     = "#0db9d7";
-static const char col_br_white[]    = "#acb0d0";
-
 static const char *colors[][3]           = {
 	/*                     fg              bg              border   */
     [SchemeNorm]       = { gray3,   black,  gray2 },
@@ -68,7 +38,7 @@ static const char *colors[][3]           = {
 };
 
 /* tagging */
-static char *tags[] = {"", "", "", "", "", "", "", "漣", ""};
+static char *tags[] = {"1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 "};
 
 static const int tagschemes[] = {
         SchemeTag1, SchemeTag2, SchemeTag3,
@@ -159,7 +129,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
     { MODKEY|ControlMask,           XK_comma,  cyclelayout,    {.i = -1 } },
     { MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
-    { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[11]} },
+    { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[12]} },
     { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[8]} },
 
 	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
